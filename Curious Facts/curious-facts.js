d43fa7c1e33fcee1579fbcfa256a2dbf68aefa5f
majoz-t <majozta@gmail.com>
2025-12-03 21:04:11 +0100

    Call and Show the curious fact

--- Curious Facts/curious-facts.js
+++ Curious Facts/curious-facts.js
@@ -1,6 +1,93 @@
-async function curiousfact() {
+
+/*const curiousUrl= "https://uselessfacts.jsph.pl/api/v2/facts/random"
+const Facts= document.querySelector(".view")
+
+
+
+fetch(curiousUrl)
+    .then(data => data.json())
+    //.then(data=>console.log(data))
+    .then(response => Facts.innerHTML= response.text)
+    //.then(response => console.log(response))
+    
+    //.then(fact => Facts.innerHTML = fact)
+
+function getAFact(){
+    const button = document.querySelector(".clickfact");
+    button.addEventListener("click", fetch);
+}
+*/
+
+/*async function curiousfact() {
     let response= await fetch ("https://uselessfacts.jsph.pl/ ");
     let fact= await response.json()
 
     console.log(fact)
 }   
+
+curiousFact();*/
+
+/*const curiousUrl = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+const Facts = document.querySelector(".view");
+function getAFact() {
+  fetch(curiousUrl, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(data => {
+    Facts.innerHTML = data.text;
+  })
+  .catch(err => console.error(err));
+
+
+console.log(Facts)
+}*/
+//document.querySelector(".clickfact").addEventListener("click", getAFact);
+
+/*async function curiousfact() {
+    const curiousUrl= "https://uselessfacts.jsph.pl/api/v2/facts/random"
+    const resultfetch= await fetch(curiousUrl)
+    const data= await resultfetch.json()
+    console.log(data)
+    return data.text
+}*/
+
+console.log("prueba")
+
+const curiousUrl= "https://uselessfacts.jsph.pl/api/v2/facts/random";
+const Facts= document.querySelector(".view")
+
+function getAFact(){
+    fetch(curiousUrl)
+        .then((response) => response.json())
+        .then((cFs) => Facts.textContent= cFs.text)
+    }
+
+const getbutton=document.getElementById("clickfact");
+getbutton.addEventListener("click", getAFact);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
