c0174eb7b56c0010c6c77449b2f05ac85aa39484
majoz-t <majozta@gmail.com>
2025-12-03 23:38:58 +0100

    Favorite List

--- Curious Facts/curious-facts.js
+++ Curious Facts/curious-facts.js
@@ -56,8 +56,11 @@ console.log(Facts)
 
 console.log("prueba")
 
+
 const curiousUrl= "https://uselessfacts.jsph.pl/api/v2/facts/random";
-const Facts= document.querySelector(".view")
+const Facts= document.querySelector(".view");
+let favoriteList=[];
+const favoriteFact=document.getElementById("favorite-List");
 
 function getAFact(){
     fetch(curiousUrl)
@@ -67,6 +70,28 @@ function getAFact(){
 
 const getbutton=document.getElementById("clickfact");
 getbutton.addEventListener("click", getAFact);
+
+
+function saveFavorites(){
+    const currentFact= Facts.textContent;
+    if(currentFact.trim() !== ' ') {
+        favoriteList.push(currentFact);
+        console.log("favoritos", favoriteList)   
+        addFavorites(favoriteFact);  
+    }
+}
+
+function addFavorites(){
+    favoriteFact.innerHTML=' ';
+    favoriteList.forEach(fact => {
+        const li= document.createElement("li");
+        li.textContent=fact;
+        favoriteFact.appendChild(li);
+    })
+}
+
+const favoritebotton= document.getElementById("buttonloveit");
+favoritebotton.addEventListener("click", saveFavorites);
 
 
     
